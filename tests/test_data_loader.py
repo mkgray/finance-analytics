7b40ad4b7cd2ccec4a318a120ab13cdef5725301
Matt Gray <blampz@hotmail.com>
2022-10-16 22:02:30 -0400

    Added unit tests for structured loading function

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,6 +1,7 @@
 import unittest
 from financeanalytics import dataloader
 from pathlib import Path
+import pandas as pd
 
 class TestDataLoader(unittest.TestCase):
 
@@ -16,9 +17,9 @@ class TestDataLoader(unittest.TestCase):
 
         root_folder = "tests/detection_test"
 
-        output = DataLoader._detect_relevant_files(root_folder)
+        actual_output = DataLoader._detect_relevant_files(root_folder)
 
-        self.assertEqual(expected_output, output)
+        self.assertEqual(expected_output, actual_output)
 
     def test_detect_relevant_files_default_value(self):
         input_files = ["root_folder/RBC/Chequing/empty.pdf",
@@ -39,9 +40,9 @@ class TestDataLoader(unittest.TestCase):
 
         DataLoader = dataloader.DataLoader()
 
-        output = DataLoader._remove_unsupported_banks(proper_input)
+        actual_output = DataLoader._remove_unsupported_banks(proper_input)
 
-        self.assertEqual(expected_output, output)
+        self.assertEqual(expected_output, actual_output)
 
     def test_detect_relevant_files_multi_bank(self):
         input_files = ["root_folder/RBC/Chequing/empty.pdf",
@@ -64,9 +65,9 @@ class TestDataLoader(unittest.TestCase):
 
         DataLoader = dataloader.DataLoader(supported_banks=["rbc", "td"])
 
-        output = DataLoader._remove_unsupported_banks(proper_input)
+        actual_output = DataLoader._remove_unsupported_banks(proper_input)
 
-        self.assertEqual(expected_output, output)
+        self.assertEqual(expected_output, actual_output)
 
     def test_filter_files_missing_dates(self):
         input_files = ["root_folder/RBC/Chequing/Chequing Statement-9999 2000-01-01.pdf",
@@ -84,9 +85,118 @@ class TestDataLoader(unittest.TestCase):
 
         DataLoader = dataloader.DataLoader()
 
-        output = DataLoader._remove_files_missing_dates(proper_input)
+        actual_output = DataLoader._remove_files_missing_dates(proper_input)
 
-        self.assertEqual(expected_output, output)
+        self.assertEqual(expected_output, actual_output)
+
+    def test_structure_data_1(self):
+        # Testing for mixed scenario
+        input_files = ["root_folder/RBC/GroupA/GroupA-1/Statement 2000-01-01.pdf",
+                       "root_folder/RBC/GroupA/GroupA-1/Statement 2000-02-01.pdf",
+                       "root_folder/RBC/GroupA/GroupA-2/Statement 2000-01-01.pdf",
+                       "root_folder/RBC/GroupA/GroupA-2/Statement 2000-02-01.pdf",
+                       "root_folder/RBC/GroupB/Statement 2000-01-01.pdf",
+                       "root_folder/RBC/GroupB/Statement 2000-02-01.pdf",
+                       "root_folder/TD/GroupA/GroupA-1/Statement 2000-01-01.pdf",
+                       "root_folder/TD/GroupA/GroupA-1/Statement 2000-02-01.pdf",
+                       "root_folder/TD/GroupA/GroupA-3/Statement 2000-01-01.pdf",
+                       "root_folder/TD/GroupA/GroupA-3/Statement 2000-02-01.pdf"
+                       ]
+
+        proper_input = [Path(x) for x in input_files]
+
+        output_data = [["RBC", "GROUPA", "GROUPA-1", "root_folder/RBC/GroupA/GroupA-1/Statement 2000-01-01.pdf"],
+                       ["RBC", "GROUPA", "GROUPA-1", "root_folder/RBC/GroupA/GroupA-1/Statement 2000-02-01.pdf"],
+                       ["RBC", "GROUPA", "GROUPA-2", "root_folder/RBC/GroupA/GroupA-2/Statement 2000-01-01.pdf"],
+                       ["RBC", "GROUPA", "GROUPA-2", "root_folder/RBC/GroupA/GroupA-2/Statement 2000-02-01.pdf"],
+                       ["RBC", "GROUPB", None, "root_folder/RBC/GroupB/Statement 2000-01-01.pdf"],
+                       ["RBC", "GROUPB", None, "root_folder/RBC/GroupB/Statement 2000-02-01.pdf"],
+                       ["TD", "GROUPA", "GROUPA-1", "root_folder/RBC/GroupA/GroupA-1/Statement 2000-01-01.pdf"],
+                       ["TD", "GROUPA", "GROUPA-1", "root_folder/RBC/GroupA/GroupA-1/Statement 2000-02-01.pdf"],
+                       ["TD", "GROUPA", "GROUPA-3", "root_folder/RBC/GroupA/GroupA-3/Statement 2000-01-01.pdf"],
+                       ["TD", "GROUPA", "GROUPA-3", "root_folder/RBC/GroupA/GroupA-3/Statement 2000-02-01.pdf"],
+                       ]
+
+        expected_output = pd.DataFrame(output_data, columns=["Bank", "Level1", "Level2", "Filepath"])
+
+        DataLoader = dataloader.DataLoader()
+
+        actual_output = DataLoader._structure_cleaned_file_listing(proper_input)
+
+        self.assertEqual(expected_output, actual_output)
+
+    def test_structure_data_2(self):
+        # Testing for case insensitivity
+        input_files = ["root_folder/RBC/GroupA/GroupA-1/Statement 2000-01-01.pdf",
+                       "root_folder/RBC/GroupA/groupa-1/Statement 2000-02-01.pdf",
+                       "root_folder/rBC/GroupA/GroupA-1/Statement 2000-03-01.pdf",
+                       "root_folder/rbc/groupa/GroupA-1/Statement 2000-04-01.pdf"
+                       ]
+
+        proper_input = [Path(x) for x in input_files]
+
+        output_data = [["RBC", "GROUPA", "GROUPA-1", "root_folder/RBC/GroupA/GroupA-1/Statement 2000-01-01.pdf"],
+                       ["RBC", "GROUPA", "GROUPA-1", "root_folder/RBC/GroupA/GroupA-1/Statement 2000-02-01.pdf"],
+                       ["RBC", "GROUPA", "GROUPA-1", "root_folder/RBC/GroupA/GroupA-1/Statement 2000-03-01.pdf"],
+                       ["RBC", "GROUPA", "GROUPA-1", "root_folder/RBC/GroupA/GroupA-1/Statement 2000-04-01.pdf"]
+                       ]
+
+        expected_output = pd.DataFrame(output_data, columns=["Bank", "Level1", "Level2", "Filepath"])
+
+        DataLoader = dataloader.DataLoader()
+
+        actual_output = DataLoader._structure_cleaned_file_listing(proper_input)
+
+        self.assertEqual(expected_output, actual_output)
+
+    def test_structure_data_3(self):
+        # Testing for no level hierarchy only bank folder
+        input_files = ["root_folder/RBC/Statement 2000-01-01.pdf",
+                       "root_folder/RBC/Statement 2000-02-01.pdf"
+                       ]
+
+        proper_input = [Path(x) for x in input_files]
+
+        output_data = [["RBC", "root_folder/RBC/Statement 2000-01-01.pdf"],
+                       ["RBC", "root_folder/RBC/Statement 2000-02-01.pdf"]
+                       ]
+
+        expected_output = pd.DataFrame(output_data, columns=["Bank", "Filepath"])
+
+        DataLoader = dataloader.DataLoader()
+
+        actual_output = DataLoader._structure_cleaned_file_listing(proper_input)
+
+        self.assertEqual(expected_output, actual_output)
+
+    def test_structure_data_4(self):
+        # Testing for many files on many levels
+        input_files = ["root_folder/RBC/A/B/C/D/E/Statement 2000-01-01.pdf",
+                       "root_folder/RBC/A/B/C/D/Statement 2000-02-01.pdf",
+                       "root_folder/RBC/A/B/C/Statement 2000-03-01.pdf",
+                       "root_folder/RBC/A/B/Statement 2000-04-01.pdf",
+                       "root_folder/RBC/A/Statement 2000-05-01.pdf",
+                       "root_folder/RBC/Statement 2000-06-01.pdf",
+                       ]
+
+        proper_input = [Path(x) for x in input_files]
+
+        output_data = [["RBC", "A", "B", "C", "D", "E", "root_folder/RBC/GroupA/GroupA-1/Statement 2000-01-01.pdf"],
+                       ["RBC", "A", "B", "C", "D", None, "root_folder/RBC/GroupA/GroupA-1/Statement 2000-02-01.pdf"],
+                       ["RBC", "A", "B", "C", None, None, "root_folder/RBC/GroupA/GroupA-1/Statement 2000-03-01.pdf"],
+                       ["RBC", "A", "B", None, None, None, "root_folder/RBC/GroupA/GroupA-1/Statement 2000-04-01.pdf"],
+                       ["RBC", "A", None, None, None, None, "root_folder/RBC/GroupA/GroupA-1/Statement 2000-05-01.pdf"],
+                       ["RBC", None, None, None, None, None, "root_folder/RBC/GroupA/GroupA-1/Statement 2000-06-01.pdf"]
+                       ]
+
+        expected_output = pd.DataFrame(output_data, columns=["Bank", "Level1", "Level2", "Level3", "Level4", "Level5",
+                                                             "Filepath"])
+
+        DataLoader = dataloader.DataLoader()
+
+        actual_output = DataLoader._structure_cleaned_file_listing(proper_input)
+
+        self.assertEqual(expected_output, actual_output)
 
 if __name__ == '__main__':
     unittest.main()

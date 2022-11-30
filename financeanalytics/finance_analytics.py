9837ec7ae98602ddaa0c906a1c003e0e122d729c
Matt Gray <blampz@hotmail.com>
2022-11-29 20:04:37 -0500

    Rough draft of GUI

---
+++ financeanalytics/finance_analytics.py
@@ -0,0 +1,80 @@
+from financeanalytics.dataloader import DataLoader
+from financeanalytics.dataquality import DataQuality
+from financeanalytics.statementprocessor import StatementProcessor
+
+from pathlib import Path
+
+import logging
+from tqdm import tqdm
+
+import pandas as pd
+
+import argparse
+
+class FinanceAnalytics:
+
+    def determine_statement_type(self, pdf_filepath):
+        if 'chequing' in str(pdf_filepath).lower():
+            return 'Chequing'
+        elif 'visa' in str(pdf_filepath).lower():
+            return 'Visa'
+        else:
+            raise NameError('Filepath does not specify chequing or visa statement type explicitly, please include')
+
+    def extract_statement_data(self, df_record):
+
+        pdf_filepath = df_record["Filepath"]
+        bank = df_record["Bank"]
+        statement_type = self.determine_statement_type(pdf_filepath)
+        return StatementProcessor().extract_with_validation(pdf_filepath, bank, statement_type)
+
+    def extract_all_statements(self, structured_data):
+        total_records = structured_data.shape[0]
+        column_names = list(structured_data.columns)
+
+        df_all_statements = []
+
+        for index, row in tqdm(structured_data.iterrows(), total=structured_data.shape[0]):
+            pdf_filepath = row["Filepath"]
+            bank = row["Bank"]
+            statement_type = self.determine_statement_type(pdf_filepath)
+
+            statement_df = StatementProcessor().extract_with_validation(pdf_filepath, bank, statement_type)
+
+            # Append the hierarchy onto the results
+            statement_df[column_names] = row.values
+
+            # Add the statement with hierarchy to the complete dataset
+            df_all_statements.append(statement_df)
+
+            #logging.warning("Processed file {idx} of {max_idx}, ({prcnt}% Complete): {fname}".format(idx=index + 1, max_idx=total_records, prcnt=round((float(index + 1) / total_records) * 100, 2), fname=pdf_filepath))
+
+        # Merge statements into one dataframe
+        return pd.concat(df_all_statements, axis=0).reset_index(drop=True)
+
+    def write_output_to_location(self, all_statements, root_location):
+        output_path = Path(root_location + '/extracted_transactions.xlsx')
+        all_statements.to_excel(output_path, index=False)
+
+    def __init__(self, root_input_folder):
+        logging.info("Finance Analytics target location: " + root_input_folder)
+
+        # Load the data
+        structured_data = DataLoader().load_data(root_input_folder)
+
+        # Run the DQ analysis
+        DataQuality().analyze_data_quality(structured_data)
+
+        # Extract the statements and write
+        self.write_output_to_location(self.extract_all_statements(structured_data), root_input_folder)
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.WARNING)
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--root_folder', type=str, required=True)
+
+    args = parser.parse_args()
+
+    complete_df = FinanceAnalytics(args.root_folder)
--- financeanalytics/main.py
+++ financeanalytics/main.py
@@ -1,3 +1,8 @@
+from tkinter import Tk, Button, Label, ttk
+from tkinter.filedialog import askdirectory
+
+from finance_analytics import FinanceAnalytics
+
 from financeanalytics.dataloader import DataLoader
 from financeanalytics.dataquality import DataQuality
 from financeanalytics.statementprocessor import StatementProcessor
@@ -11,70 +16,55 @@ import pandas as pd
 
 import argparse
 
-class FinanceAnalytics:
 
-    def determine_statement_type(self, pdf_filepath):
-        if 'chequing' in str(pdf_filepath).lower():
-            return 'Chequing'
-        elif 'visa' in str(pdf_filepath).lower():
-            return 'Visa'
-        else:
-            raise NameError('Filepath does not specify chequing or visa statement type explicitly, please include')
+root = Tk()
+root.title("Finance Analytics")
+root.iconbitmap("../assets/icons/finance_analytics_logo.ico")
 
-    def extract_statement_data(self, df_record):
+def get_directory():
+    global root_dir
+    root_dir = askdirectory(title="Choose Root Folder for Financial Statement Processing")
+    label_root_folder.config(text="Root Folder: {}".format(root_dir))
 
-        pdf_filepath = df_record["Filepath"]
-        bank = df_record["Bank"]
-        statement_type = self.determine_statement_type(pdf_filepath)
-        return StatementProcessor().extract_with_validation(pdf_filepath, bank, statement_type)
+    # Output directory by default is the same as the root directory
+    global output_dir
+    output_dir = root_dir
+    label_output_location.config(text="Output Location: {}/extracted_transactions.xlsx".format(root_dir))
 
-    def extract_all_statements(self, structured_data):
-        total_records = structured_data.shape[0]
-        column_names = list(structured_data.columns)
+    # Enable output directory choosing and processing functions once input location determined
+    #button_choose_out.config(state="normal") # disabled until feature is completed
+    button_run.config(state="normal")
 
-        df_all_statements = []
+def get_output():
+    output_dir = askdirectory(title="Choose Output Folder for Storing Processed Results")
+    label_output_location.config(text="Output Location: {}/extracted_transactions.xlsx".format(root_dir))
 
-        for index, row in tqdm(structured_data.iterrows(), total=structured_data.shape[0]):
-            pdf_filepath = row["Filepath"]
-            bank = row["Bank"]
-            statement_type = self.determine_statement_type(pdf_filepath)
+def run_processing():
+    FinanceAnalytics(root_dir)
 
-            statement_df = StatementProcessor().extract_with_validation(pdf_filepath, bank, statement_type)
+# Initialize Buttons
+button_choose_root = Button(root, text="Choose Root Folder", command=get_directory, padx=20)
+button_choose_out = Button(root, text="Choose Output Location", command=get_output, state="disabled", padx=7)
+button_run = Button(root, text="Run", command=run_processing, state="disabled", padx=15)
+button_quit = Button(root, text="Quit", command=root.quit, padx=13)
 
-            # Append the hierarchy onto the results
-            statement_df[column_names] = row.values
+# Initialize Labels
+label_root_folder = Label(root, text="Root Folder: ", anchor="w")
+label_output_location = Label(root, text="Output Location: ", anchor="w")
 
-            # Add the statement with hierarchy to the complete dataset
-            df_all_statements.append(statement_df)
+# Initialize Progress Bar
+progress_bar = ttk.Progressbar(root, orient="horizontal", length=200, mode="determinate")
 
-            #logging.warning("Processed file {idx} of {max_idx}, ({prcnt}% Complete): {fname}".format(idx=index + 1, max_idx=total_records, prcnt=round((float(index + 1) / total_records) * 100, 2), fname=pdf_filepath))
+# Place widgets
+button_choose_root.grid(row=0, column=0, columnspan=2)
+button_choose_out.grid(row=1, column=0, columnspan=2)
+button_run.grid(row=2, column=0)
+button_quit.grid(row=2, column=4)
 
-        # Merge statements into one dataframe
-        return pd.concat(df_all_statements, axis=0).reset_index(drop=True)
+label_root_folder.grid(row=0, column=2, columnspan=3)
+label_output_location.grid(row=1, column=2, columnspan=3)
 
-    def write_output_to_location(self, all_statements, root_location):
-        output_path = Path(root_location + '/extracted_transactions.xlsx')
-        all_statements.to_excel(output_path, index=False)
+progress_bar.grid(row=2, column=1, columnspan=3)
 
-    def __init__(self, root_input_folder):
-        logging.info("Finance Analytics target location: " + root_input_folder)
-
-        # Load the data
-        structured_data = DataLoader().load_data(root_input_folder)
-
-        # Run the DQ analysis
-        DataQuality().analyze_data_quality(structured_data)
-
-        # Extract the statements and write
-        self.write_output_to_location(self.extract_all_statements(structured_data), root_input_folder)
-
-if __name__ == '__main__':
-    logging.getLogger().setLevel(logging.WARNING)
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--root_folder', type=str, required=True)
-
-    args = parser.parse_args()
-
-    complete_df = FinanceAnalytics(args.root_folder)
+# Run main loop
+root.mainloop()
